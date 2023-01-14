23adba6a129003e770db94b724871a9beebde7f9
Alex Singh <115970252+singhalex@users.noreply.github.com>
2023-01-13 17:27:54 -0800

    Set up webpack and create main page content

---
+++ src/mainPage.js
@@ -0,0 +1,19 @@
+import MainImage from './images/main.jpg';
+
+const mainImage = document.createElement('img');
+mainImage.src = MainImage;
+
+// Create the title element
+const restaurantName = 'Le Jardin des Delices';
+const title = document.createElement('h1');
+title.textContent = restaurantName;
+
+const descriptionText = "Welcome to Le Jardin des Delices, where you can indulge in delicious cuisine and impeccable service. Our menu features a variety of gourmet dishes made with the freshest ingredients and expertly prepared by our skilled chefs. Whether you're looking for a romantic dinner for two or a special occasion with friends and family, we offer a warm and elegant atmosphere that is sure to impress. Join us for a night of culinary delights and memories that will last a lifetime.";
+const description = document.createElement('p');
+description.textContent = descriptionText;
+
+export default function buildMainPage() {
+  document.body.appendChild(mainImage);
+  document.body.appendChild(title);
+  document.body.appendChild(description);
+}
