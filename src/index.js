f5055bf8c300666b35ffc498821e18da51ac7690
Alex Singh <115970252+singhalex@users.noreply.github.com>
2023-01-16 15:28:25 -0800

    Add clear page functionality to navigation links
    When the navigation bar links are clicked, the content of the page is
    cleared out. It will then rebuild the page with the appropriate content
    once properly implemented.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,28 @@ import './style.css';
 import createNavBar from './navBar';
 import buildMainPage from './mainPage';
 
-// createPage();
+// Load main page
 createNavBar();
 buildMainPage();
+
+const content = document.getElementById('content');
+function clearPage() {
+  content.innerHTML = '';
+}
+
+const home = document.getElementById('home');
+const menu = document.getElementById('menu');
+const contact = document.getElementById('contact');
+
+home.addEventListener('click', () => {
+  clearPage();
+  buildMainPage();
+});
+
+menu.addEventListener('click', () => {
+  clearPage();
+});
+
+contact.addEventListener('click', () => {
+  clearPage();
+});
--- src/navBar.js
+++ src/navBar.js
@@ -3,12 +3,15 @@ navBar.setAttribute('id', 'nav-bar');
 
 const home = document.createElement('li');
 home.textContent = 'Home';
+home.setAttribute('id', 'home');
 
 const menu = document.createElement('li');
 menu.textContent = 'Menu';
+menu.setAttribute('id', 'menu');
 
 const contact = document.createElement('li');
 contact.textContent = 'Contact';
+contact.setAttribute('id', 'contact');
 
 export default function createNavBar() {
   navBar.appendChild(home);
